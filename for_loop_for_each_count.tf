64913c99edb7c039180e71e19c749b81959792a5
durga049 <100748026+durga049@users.noreply.github.com>
2024-01-05 21:35:06 +0530

    Creating for_loop_for_each_count

---
+++ for_loop_for_each_count.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_user" "default" {
+count = length(var.user_name)
+name = var.user_name[count.index]
+}
+variable user_name  {
+type = list(string)
+default = ["user1", "user2", "user3"]
+}
+
+output "user_details" {
+description = "using for loop"
+value = [for user in var.user_name : user]
+}
+
+resource "aws_s3_bucket" "default" {
+for_each = var.bucket_name
+bucket = each.value
+}
+variable "bucket_name" {
+type = set(string)
+default = ["mybucketterra1", "mybucketterra2", "mybucketterra2"]
+}
